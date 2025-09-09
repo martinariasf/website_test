6b88d067ad651ff68f8ca9915cf992de121b9714
Martin Arias <108520947+martinariasf@users.noreply.github.com>
2025-09-09 16:44:11 +0200

    deleting not needed files

--- sites/site-1757428535876-98l92h/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const text = document.querySelector('.main-text');
-    
-    text.addEventListener('click', function() {
-        text.style.color = '#' + Math.floor(Math.random()*16777215).toString(16);
-    });
-    
-    console.log('Oh yeah website loaded!');
-});
